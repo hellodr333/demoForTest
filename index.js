9d6e45cc9ab69e707522596b2bbe93db32865bd2
chenwewe <chenwewe@outlook.com>
2017-02-08 01:13:16 +0800

    log

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 (function(win){
 	function change(){
 		// doc.documentElement.style.fontSize=doc.documentElement.clientWidth/320*20+'px';	
-		document.getElementsByTagName('html')[0].style.fontSize=document.documentElement.clientWidth/320*20+'px';
+		document.getElementsByTagName('html')[0].style.fontSize=win.innerWidth /320*20+'px';
 	}
 	change();
 	win.addEventListener('resize',change,false);
