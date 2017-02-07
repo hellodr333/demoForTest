ce70f10385388a330f0fb66d2cb429564b2e15b8
chenwewe <chenwewe@outlook.com>
2017-02-08 01:03:47 +0800

    log

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-(function(win,doc){
+(function(win){
 	function change(){
-		doc.documentElement.style.fontSize=doc.documentElement.clientWidth/320*20+'px';	
+		// doc.documentElement.style.fontSize=doc.documentElement.clientWidth/320*20+'px';	
+		document.getElementsByTagName('html')[0].style.fontSize=document.documentElement.clientWidth/320*20+'px';
 	}
 	change();
 	win.addEventListener('resize',change,false);
-})(window,document);
+})(window);
